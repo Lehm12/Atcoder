e61c6f1de86bdd735bf6fc3fcb0a8bec6163b947
Lehm12 <afda8754@chiba-u.jp>
2021-09-11 22:50:38 +0900

    ABC218

---
+++ ABC218/B.py
@@ -0,0 +1,20 @@
+import math
+import itertools
+from collections import deque, Counter, defaultdict
+import sys
+#import numpy as np
+#from decimal import Decimal
+mod = 10**9+7
+sys.setrecursionlimit(10 ** 6)
+def I(): return int(input())
+def LI(): return list(map(int,input().split()))
+def MI(): return map(int,input().split())
+def LLI(n): return [list(map(int, input().split())) for _ in range(n)]
+#G = [[] for _in range(n)]
+
+p = LI()
+ans = ''
+for i in p:
+    ans += str.lower(chr(64+i))
+print(ans)
+
